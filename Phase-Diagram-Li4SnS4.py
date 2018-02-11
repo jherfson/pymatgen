1a07c6b89805207c19fcd0e422c461f9c4e26bc1
jherfson <jherfson.castro@gmail.com>
2018-02-11 18:55:55 -0300

    The test phase diagram

---
+++ Phase-Diagram-Li4SnS4.py
@@ -0,0 +1,36 @@
+from pymatgen.ext.matproj import MPRester
+from pymatgen.apps.borg.hive import VaspToComputedEntryDrone
+from pymatgen.apps.borg.queen import BorgQueen
+from pymatgen.entries.compatibility import MaterialsProjectCompatibility
+from pymatgen.analysis.phase_diagram import PhaseDiagram
+from pymatgen.analysis.phase_diagram import PDPlotter
+
+
+#from matplotlib.font_manager import FontProperties
+
+
+# Assimilate VASP calculations into ComputedEntry object. Let's assume that
+# the calculations are for a series of new LixSnySz phases that we want to
+# know the phase stability.
+drone = VaspToComputedEntryDrone()
+queen = BorgQueen(drone, rootpath=".")
+entries = queen.get_data()
+
+# Obtain all existing Li-Sn-S phases using the Materials Project REST API
+with MPRester("Fvlb5EsNq71JxDy3") as m:
+    mp_entries = m.get_entries_in_chemsys(["Li", "Sn", "S"])
+
+# Combined entry from calculated run with Materials Project entries
+entries.extend(mp_entries)
+
+# Process entries using the MaterialsProjectCompatibility
+compat = MaterialsProjectCompatibility()
+entries = compat.process_entries(entries)
+
+# Generate and plot Li-Sn-S phase diagram
+
+pd = PhaseDiagram(entries)
+plotter = PDPlotter(pd, color='black', markerfacecolor='c', markersize=15, linewidth = 1)
+#plotter.show()
+#print(entries)
+print(pd)
